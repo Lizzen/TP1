6e114ffe1b7c018e473c19bb2275608da3da045a
DEIVISS <99610648+Lizzen@users.noreply.github.com>
2024-12-01 23:57:04 +0100

    update command parse exceptions

--- src/tp1/exceptions/CommandParseException.java
+++ src/tp1/exceptions/CommandParseException.java
@@ -6,4 +6,8 @@ public class CommandParseException extends CommandException{
 	public CommandParseException (String descripcion) {
 		super (descripcion);
 	}
+	
+	public CommandParseException (String descripcion, Throwable cause) {
+		super (descripcion, cause);
+	}
 }
